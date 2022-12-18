e3a06aad76fa2d4f197152451844df1ccc92319c
Willian Gonçalves Rios <71411461+williangrios@users.noreply.github.com>
2022-12-18 10:15:58 -0300

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useEffect, useState } from 'react';
+import "react-toastify/dist/ReactToastify.css";
+import './App.css';
+
+import {  useState, useEffect } from 'react';
+import { ethers } from "ethers";
+import {ToastContainer, toast} from "react-toastify";
+
 import WRHeader from 'wrcomponents/dist/WRHeader';
 import WRFooter from 'wrcomponents/dist/WRFooter';
 import WRInfo from 'wrcomponents/dist/WRInfo';
 import WRContent from 'wrcomponents/dist/WRContent';
 import WRTools from 'wrcomponents/dist/WRTools';
-import { ethers } from "ethers";
-import './App.css';
-import {ToastContainer, toast} from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { _toEscapedUtf8String } from "ethers/lib/utils";
+
 
 function App() {
 
@@ -20,8 +22,9 @@ function App() {
   const [totalValue, setTotalValue] = useState(0);
   const [listTo, setListTo] = useState([]);
   const [listAmount, setListAmount] = useState([]);
+  const [userAccount, setUserAccount] = useState('');
 
-  const addressContract = '0x14dD69e10a3c1C26F03672A57670ED214dcbA3BA';
+  const contractAddress = '0x96CDa6BcCE4d44964Ce597e073A14F0DbfdbbFF8';
 
   const abi = [
     {
@@ -62,40 +65,35 @@ function App() {
       "type": "function",
       "payable": true
     }
-  ];
+  ]; 
 
   let contractDeployed = null;
   let contractDeployedSigner = null;
-  
-  async function connectMetaMask (){
-    if(typeof window.ethereum !== "undefined"){
-        try
-        {
-            let resp = await window.ethereum.request({ method: "eth_requestAccounts" });
-            setAddressConnected(resp[0]);
-            setConnected(true)
-            toastMessage("You are connected");
-        }
-        catch (error) {
-            console.log(error);
-            setConnected(false);
-            toastMessage("Oops.. An error, sorry..");
-        }
-    }
-    else {
-        setConnected(false)
-        toastMessage("Conect your metamask.")
+
+  async function getProvider(connect = false){
+    try {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      if (contractDeployed == null){
+        contractDeployed = new ethers.Contract(contractAddress, abi, provider)
       }
+      if (contractDeployedSigner == null){
+        contractDeployedSigner = new ethers.Contract(contractAddress, abi, provider.getSigner());
+      }
+      if (connect && userAccount==''){
+        let userAcc = await provider.send('eth_requestAccounts', []);
+        setUserAccount(userAcc[0]);
+      }  
+    } catch (error) {
+      toastMessage(error.reason)
+    }
   }
 
-  function getProvider(){
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    if (contractDeployed == null){
-      contractDeployed = new ethers.Contract(addressContract, abi, provider)
-    }
-    if (contractDeployedSigner == null){
-      contractDeployedSigner = new ethers.Contract(addressContract, abi, provider.getSigner());
-    }
+  useEffect(() => {
+    getData()
+  }, [])
+
+  async function getData(connect = false) {
+    await getProvider(connect);
   }
 
   function toastMessage(text) {
@@ -117,15 +115,19 @@ function App() {
       toastMessage("Add at least one address to the list.");
       return;
     }
-    console.log(listTo.length);
-    console.log(listAmount.length);
-    console.log(addressConnected);
-    getProvider();
-    const resp = await contractDeployedSigner.send( listTo, listAmount, {from: addressConnected, value: (totalValue).toString()});
-    toastMessage (`Deposited`);
-    setListAmount([])
-    setListTo([])
-    setTotalValue(0);
+
+    await getProvider(true);
+
+    try {
+        const resp = await contractDeployedSigner.send( listTo, listAmount, {from: addressConnected, value: (totalValue).toString()});
+        toastMessage (`Deposited`);
+        setListAmount([])
+        setListTo([])
+        setTotalValue(0);
+      } catch (error) {
+        toastMessage(error.reason);
+      }
+
   }
 
   return (
@@ -135,13 +137,13 @@ function App() {
       <WRInfo chain="Goerli testnet" />
       <WRContent>
 
-        {connected ? 
+        {userAccount != '' ? 
           <>
           <h2>Your address: {addressConnected}</h2>
           <h3>Add to list</h3>
-          <input type="text" placeholder="Address to" onChange={(e) => setTo(e.target.value)} value={to}/>
-          <input type="text" placeholder="Value" onChange={(e) => setValue(e.target.value)} value={value}/>
-          <button onClick={() => handleAdd(to, value)}>Add in list</button>
+          <input type="text" className="col-3" placeholder="Address to" onChange={(e) => setTo(e.target.value)} value={to}/>
+          <input type="number" className="mb-1 col-3" placeholder="Value" onChange={(e) => setValue(e.target.value)} value={value}/>
+          <button className="btn btn-primary col-3" onClick={() => handleAdd(to, value)}>Add in list</button>
           <hr/>
           <h3>Payment List</h3>
           {listTo.length !=0 ? <>
@@ -152,22 +154,18 @@ function App() {
               ))
             }
             </ul>
-            <span>Total value: {totalValue}</span></>
+            <label>Total value: {totalValue}</label></>
             :
-            <span>Empty List</span>
+            <label>Empty List</label>
           }
           
           <hr/>
           <h3>Pay</h3>
-          <button onClick={handleDeposit}>Deposit funds</button>        </>
+          <button className="btn btn-primary col-3" onClick={handleDeposit}>Deposit funds</button>        </>
         : <>
-          <button onClick={connectMetaMask}>Connect your metamask</button>
+          <button className="btn btn-primary col-3" onClick={getData}>Start</button>
         </>  
         }
-
-       
-       
-
       </WRContent>
       <WRTools react={true} truffle={true} bootstrap={true} solidity={true} css={true} javascript={true} ganache={true} ethersjs={true} />
       <WRFooter />    
